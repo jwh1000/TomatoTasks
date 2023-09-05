01299c93977480686fb3bd65bc92259300ce2ef5
jwh1000 <jwh1000@rit.edu>
2023-09-05 12:58:55 -0400

    added basic functionality for switching between clock modes

---
+++ main.js
@@ -0,0 +1,61 @@
+/**
+ * Timer variable stores the times needed for each pomodoro and break length
+ */
+const timer = {
+    pomodor: 25,
+    shortBreak: 5,
+    longBreak: 15,
+    longBreakInterval: 4,
+};
+
+
+/**
+ * initialization; grabs the buttons from HTML and adds an event listener for 
+ * when they are clicked
+ */
+const modeButtons = document.querySelector('#js-mode-buttons');
+modeButtons.addEventListener('click', handleMode);
+
+
+/**
+ * Updates the numbers for the clock to display the remaining time
+ * Pads the display with 0's for accurate time representation
+ */
+function updateClock() {
+    const {remainingTime} = timer;
+    const minutes = '${remainingTime.minutes}'.padStart(2, '0');
+    const seconds = '${remainingTime.seconds}'.padStart(2, '0');
+
+    const min = document.getElementById('js-minutes');
+    const sec = document.getElementById('js-seconds');
+    min.textContent = minutes;
+    sec.textContent = seconds;
+}
+
+
+function switchMode(mode) {
+    timer.mode = mode;
+    timer.remainingTime = {
+        total: timer[mode] * 60,
+        minutes: timer[mode],
+        seconds: 0,
+    };
+
+    document
+        .querySelectorAll('button[data-mode]')
+        .forEach(e => e.classList.remove('active'));
+    document.querySelector('[data-mode="$[mode]"').classList.add('active');
+    document.body.style.backgroundColor = 'var(--${mode})';
+
+    updateClock();
+}
+
+
+function handleMode(event) {
+    const {mode} = event.target.dataset;
+
+    if (!mode) return;
+
+    switchMode(mode);
+}
+
