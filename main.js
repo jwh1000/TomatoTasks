a7d004ee3c9b9218dd8e8c32ea8dbc6ad0a91edd
jwh1000 <jwh1000@rit.edu>
2023-09-07 10:57:27 -0400

    timer now counts down from its start value. added barebones todoist API testing

--- main.js
+++ main.js
@@ -1,20 +1,56 @@
 /**
+ * author: Jack Hunsberger
+ * PomoTimer is a simple pomodoro timer app to help with studying and focus
+ */
+import apiKey from './api-key.json' assert { type: 'json' };
+
+const Http = new XMLHttpRequest();
+const url=' https://api.todoist.com/rest/v2/projects';
+
+fetch(url, {
+   headers: {
+      'Authorization': 'Bearer ' + apiKey.key
+   }
+})
+   .then(response => response.text())
+   .then(text => console.log(text))
+
+Http.onreadystatechange = (e) => {
+    console.log(Http.responseText)
+}
+
+/**
  * Timer variable stores the times needed for each pomodoro and break length
  */
 const timer = {
-    pomodoro: 25,
+    pomodoro: 1,
     shortBreak: 5,
     longBreak: 15,
     longBreakInterval: 4,
 };
+
+let interval; // 
 
 
 /**
  * initialization; grabs the buttons from HTML and adds an event listener for 
  * when they are clicked
  */
+const mainButton = window.document.getElementById('js-btn');
+mainButton.addEventListener('click', () => {
+    const {action} = mainButton.dataset;
+    if (action === 'start') {
+        startTimer();
+    }
+});
+
 const modeButtons = document.querySelector('#js-mode-buttons');
 modeButtons.addEventListener('click', handleMode);
+
+// on loading the page, default to the pomodoro mode
+document.addEventListener('DOMContentLoaded', () => {
+    switchMode('pomodoro');
+})
 
 
 /**
@@ -30,6 +66,50 @@ function updateClock() {
     const sec = document.getElementById('js-seconds');
     min.textContent = minutes;
     sec.textContent = seconds;
+}
+
+
+/**
+ * Find how much time is remaining for the timer
+ * @param {number} endTime 
+ * @returns 
+ */
+function getRemainingTime(endTime) {
+    const currentTime = Date.parse(new Date());
+    const difference = endTime - currentTime;
+
+    const total = Number.parseInt(difference / 1000, 10);
+    const minutes = Number.parseInt((total / 60) % 60, 10);
+    const seconds = Number.parseInt(total % 60, 10);
+
+    return {
+        total,
+        minutes,
+        seconds
+    };
+}
+
+/**
+ * starts counting down the timer, called when start button is pressed
+ * interval ticks every second to update the timer
+ */
+function startTimer() {
+    let {total} = timer.remainingTime;
+    const endTime = Date.parse(new Date()) + total * 1000
+    
+    mainButton.dataset.action = 'stop';
+    mainButton.textContent = 'stop';
+    mainButton.classList.add('active');
+
+    interval = setInterval(function() {
+        timer.remainingTime = getRemainingTime(endTime);
+        updateClock();
+
+        total = timer.remainingTime.total
+        if (total <= 0) {
+            clearInterval(interval);
+        }
+    }, 1000);
 }
 
 
